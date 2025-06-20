0c1ec750cf79ffaeb3419f5e5af3a06eed894b04
Assoft <macsonfor@gmail.com>
2025-06-20 03:09:32 +0300

    remove foreignkey

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->boolean("is_business")->default(false);
-            $table->foreignIdFor(\App\Models\Company::class)->nullable()->constrained()->nullOnDelete();
+            $table->unsignedBigInteger("company_id")->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
